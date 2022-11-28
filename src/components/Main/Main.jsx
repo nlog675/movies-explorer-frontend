cbc2215a0576b24a8a09080dd08164a3d5ccd680
nlog <nlog675@gmail.com>
2022-11-28 16:08:21 +0300

    add promo&navbar markup&styles

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Promo from "../Promo/Promo";
+import NavTab from "../NavTab/NavTab";
 
 function Main() {
   return (
     <main className="Main">
-
+      <Promo />
+      <NavTab />
     </main>
   )
 }
---
+++ src/components/NavTab/NavTab.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function NavTab() {
+  return (
+    <nav className="NavTab">
+      <ul className="NavTab__list">
+        <li className="NavTab__item">
+          <button className="NavTab__button">О проекте</button>
+        </li>
+        <li className="NavTab__item">
+          <button className="NavTab__button">Технологии</button>
+        </li>
+        <li className="NavTab__item">
+          <button className="NavTab__button">Студент</button>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavTab;
