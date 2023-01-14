1bc1814b8f4972da2707dd771cbb80b77e86e53e
nlog <nlog675@gmail.com>
2023-01-14 13:39:56 +0300

    change baseurl

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -93,7 +93,7 @@ class MainApi {
 }
 
 const mainApi = new MainApi({
-  baseUrl: 'https://movie-explorer-nlog.nomoredomains.rocks/',
+  baseUrl: 'https://movie-explorer-nlog.nomoredomains.rocks',
   headers: {
     'Content-Type': 'application/json',
   },
